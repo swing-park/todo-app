b50acba061a3e2c28b92442fd4fdbbb03652fa4f
dev.swing <ongsim123@gmail.com>
2023-02-08 14:22:36 +0900

    Feat :sparkles: : Add global providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,42 @@
+import styled from "styled-components";
+import { StyledEngineProvider } from "@mui/material";
 import { GlobalStyle } from "styles";
+import { Header, Board } from "component";
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+
+  width: 100%;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+  padding: 10px 0px;
+
+  border-radius: 50px;
+
+  background-color: #fff;
+  box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.5);
+
+  max-width: 1200px;
+  min-width: 800px;
+`;
 
 const App = () => {
   return (
-    <>
+    <StyledEngineProvider injectFirst>
       <GlobalStyle />
-      <div className="App">Hello world !</div>
-    </>
+      <Wrapper>
+        <Container>
+          <Header />
+          <Board />
+        </Container>
+      </Wrapper>
+    </StyledEngineProvider>
   );
 };
 
